abbada0b29b65880ec60fc09d242a3a115365c54
Szer13 <shad350@gmail.com>
2023-08-18 16:08:02 +0200

    update

--- src/main/java/pl/coderslab/entity/User.java
+++ src/main/java/pl/coderslab/entity/User.java
@@ -1,2 +1,67 @@
-package pl.coderslab.entity;public class User {
+package pl.coderslab.entity;
+
+public class User {
+    private int id;
+    private String userName;
+    private String email;
+    private String password;
+
+    public User() {
+        this(0, null, null, null);
+    }
+
+    public User(String userName, String email, String password) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(int id, String userName, String email, String password) {
+        this.id = id;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
