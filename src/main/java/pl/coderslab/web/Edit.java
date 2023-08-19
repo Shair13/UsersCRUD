6e6b1f75b461fe5271b0486e94876ef534ef99a2
Szer13 <shad350@gmail.com>
2023-08-19 12:13:29 +0200

    edit users

--- src/main/java/pl/coderslab/web/Edit.java
+++ src/main/java/pl/coderslab/web/Edit.java
@@ -1,5 +1,8 @@
 package pl.coderslab.web;
 
+import pl.coderslab.entity.User;
+import pl.coderslab.entity.UserDao;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -9,11 +12,20 @@ import java.io.IOException;
 public class Edit extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String id = request.getParameter("id");
+        if(id == null){
+            getServletContext().getRequestDispatcher("/users/incorrect.jsp").forward(request, response);
 
+        }else {
+            UserDao userDao = new UserDao();
+            User read = userDao.read(Integer.parseInt(id));
+            request.setAttribute("user", read);
+            getServletContext().getRequestDispatcher("/users/edit.jsp").forward(request, response);
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        response.sendRedirect("/user/list");
     }
 }
